78c11eadcc1a66c708c47d648af0cf64814f5de6
dokyung253-lang <dokyung253@gmail.com>
2026-02-25 10:19:08 +0900

    10:25

--- src/main/java/example/day02/sevlet/AppStart.java
+++ src/main/java/example/day02/sevlet/AppStart.java
@@ -2,11 +2,13 @@ package example.day02.sevlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 import javax.swing.*;
 @SpringBootApplication // 스프링부트 환경 의존성(미리 만들어진 코드) 주입
 // 모든 코드들은 ctrl+클릭하면 해당코드로 이동한다.
-// *WAS(웹서버) 환경 설정이 포함된다.
+// *WAS(웹서버) 환경 설정이 포함된다. ---> HTTP: http://localhost:8080
+@ServletComponentScan
 public class AppStart {
     public static void main(String[] args) {
         SpringApplication.run( AppStart.class);
@@ -15,6 +17,5 @@ public class AppStart {
         // 즉] 스프링환경 세팅이 AppStart에 연결된 상태이므로 AppStart클래스 실행
         // 클래스명.class : 클래스 정보(멤버/함수/상속/구현/생성자 등 호출)
         System.out.println("AppStart.class");
-
     }// main end
 }// class end
