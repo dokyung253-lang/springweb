e635350bb9b27bf18740a8d0453b6c8c7400234b
dokyung253-lang <dokyung253@gmail.com>
2026-02-25 10:19:08 +0900

    10:19

--- src/main/java/example/day02/sevlet/AppStart.java
+++ src/main/java/example/day02/sevlet/AppStart.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.swing.*;
 @SpringBootApplication // 스프링부트 환경 의존성(미리 만들어진 코드) 주입
 // 모든 코드들은 ctrl+클릭하면 해당코드로 이동한다.
-// *WAS(웹서버) 환경 설정이 포함된다.
+// *WAS(웹서버) 환경 설정이 포함된다. ---> HTTP: http://localhost:8080
 public class AppStart {
     public static void main(String[] args) {
         SpringApplication.run( AppStart.class);
@@ -15,6 +15,5 @@ public class AppStart {
         // 즉] 스프링환경 세팅이 AppStart에 연결된 상태이므로 AppStart클래스 실행
         // 클래스명.class : 클래스 정보(멤버/함수/상속/구현/생성자 등 호출)
         System.out.println("AppStart.class");
-
     }// main end
 }// class end
---
+++ src/main/java/example/day02/sevlet/ServletController.java
@@ -0,0 +1,31 @@
+package example.day02.sevlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+// 서블릿이란? 자바 회사에서 HTTP웹 동적처리 하기위한 라이브러리(미리 만든)/ 클래스
+// 사용법
+// 1. 현재 클래스의 "HttpServlet" 클래스로 부터 상속받는다. 상속이란? 다른 클래스로 부터 멤버변수/함수 물려받는 것이다.
+// 2. 현재클래스 위에 @WebServlet("/URL") 주소 설정한다. 중복없이 영문으로 정의한다.
+@WebServlet("/day02/servlet") // --> localhost:8080/day02/servlet 요청 시 통신 가능한 클래스 정의
+public class ServletController extends HttpServlet {
+    // 순수 자바의 메소드/함수
+    boolean method( int param ){ return true; }
+
+    @Override public void init(ServletConfig config) throws ServletException {
+        // 1. 서블릿 클래스로 부터 HTTP 요청 시 init함수 최초 1번 실행된다.
+        System.out.println("init 함수 실행");
+        super.init(config);
+    }
+
+    // 2. 서블릿 클래스로부터 HTTP 요청마다 service함수 실행된다.
+    @Override public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+}
