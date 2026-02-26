be8d2ae1b3db325bd4983899fcc37998a3c55fd2
dokyung253-lang <dokyung253@gmail.com>
2026-02-26 14:57:25 +0900

    0226 14:57

---
+++ src/main/java/example/day03/axios/AxiosController.java
@@ -0,0 +1,11 @@
+package example.day03.axios;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/day03/task")
+public class AxiosController {
+
+
+}
