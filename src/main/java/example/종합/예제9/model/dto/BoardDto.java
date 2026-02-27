c6fa14093e8b3ebf0dddf0ad0da09d4b8a9ee56d
dokyung253-lang <dokyung253@gmail.com>
2026-02-27 14:36:03 +0900

    0227 14:35

--- src/main/java/example/종합/예제9/model/dto/BoardDto.java
+++ src/main/java/example/종합/예제9/model/dto/BoardDto.java
@@ -1,4 +1,12 @@
 package example.종합.예제9.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class BoardDto {
+
 }
