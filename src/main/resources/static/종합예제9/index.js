dcd24afd78b65c345b33a3f54075e6de3e342885
dokyung253-lang <dokyung253@gmail.com>
2026-02-27 13:10:05 +0900

    0227 01:09

--- src/main/resources/static/종합예제9/index.js
+++ src/main/resources/static/종합예제9/index.js
@@ -29,7 +29,7 @@ onFindAll(); // js가 열렸을 때, 전체조회 함수 호출
 const onWrite = ( ) => {
     // 1. DOM객체 가져온다.
     const bcontentInput = document.querySelector('#bcontent');
-    const bwriterInput = document.querySelector)('#bwriter');
+    const bwriterInput = document.querySelector('#bwriter');
     // 2. 가져온 DOM객체 내 입력받은 값 꺼내기
     const bcontent = bcontentInput.value;
     const bwirter = bwriterInput.value;
@@ -37,9 +37,10 @@ const onWrite = ( ) => {
     const obj = { "bcontent" : bcontent , "bwriter" : bwriter }
     // 4. (1개월차) 배열에 저장한다 --> (3개월차) AXIOS 이용하여 서버에게 저장 요청한다.
     // 동기화 AXIOS, 1] 현재 함수 앞에 async키워드 붙인다. 2] axios 앞에 await키워드 붙인다.
-    const response = await axios.post("/board", obj }; //axios.HTTP메소드명("통신할주소", body 본문)
+    const response = await axios.post("/board", obj ) ; //axios.HTTP메소드명("통신할주소", body 본문)
+    const data = response.data; // 응답값
     if(data == true){
-        alert("등록성공"); bcontentInput.value = ", bwriterInput.value=";}
+        alert("등록성공"); 
         bcontentInput.value = ",bwriterInput.value ="; //입력상자에 입력한 값들 초기화
         onFindAll(); //** 화면 새로고침 **
         }
@@ -47,11 +48,32 @@ const onWrite = ( ) => {
     }// func end
 
 // 3] 개별 수정
+const onUpdate = async(bno) => { // 1] 수정할 번호를 매개변수로 받는다.
+    
+    // 1) 새로운 수정할 내용 입력받ㄷ기
+    const bcontent = prompt("수정할 내용");
+    // 2) 객체(Body) 구성 , 속성명 대입할 변수명이 같다면 속성명 생략
+    const obj = { bno, bcontent} // vs { "bno" : bno, "bcontent" : bcontent }
+    // 3) axios 활용하여 서버에게 수정할 내용 요청 후 응답 받기
+    const response = await axios.put("/board", obj);
+    const data = response.data;
+    // 4) 결과 제어
+    if(data == true){
+        alert("수정성공");
+        onFindAll(); // **화면 새로고침**
+    }else{ alert("수정실패");}
+} // func end
 
 // 4] 개별 삭제
 const onDelete = async(bno) => { // 1] 삭제할 번호를 매개변수로 받는다.
 
     // 2] axios 활용하여 삭제할 번호를 서버에게 쿼리스트링으로 요청하고 결과를 응답받는다.
-    const response = await.axios.delete('/board')
-
-}
+    const response = await axios.delete(`/board?bon=${bno}`);
+    const data = response.data; 
+    // 3] 결과에 따른 코드 제어
+    if(data == true){
+        alert("삭제성공");
+        onFindAll(); //** 화면 새로고침 **
+    }else{alert("삭제실패 : 관리자에게 문의 ");        
+    }
+}// f end
