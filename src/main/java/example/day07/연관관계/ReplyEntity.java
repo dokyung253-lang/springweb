57661d7fa5043479273387d19da8c404d97760ef
dokyung253-lang <dokyung253@gmail.com>
2026-03-09 12:22:03 +0900

    0309 12:21

--- src/main/java/example/day07/연관관계/ReplyEntity.java
+++ src/main/java/example/day07/연관관계/ReplyEntity.java
@@ -15,7 +15,7 @@ public class ReplyEntity {
     // ** 단방향 ** FK 만들기
     @ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY ) // Fk 제약조건 설정
     @JoinColumn(name="bno") // FK 필드명 설정
-    private BoardEnrtity boardEnrtity;
+    private BoardEnrtity boardEntity;
 }
 /*
     - 영속성이란?  자바객체와 데이터베이스 데이터 간의 매핑/연결 상태
@@ -28,9 +28,27 @@ public class ReplyEntity {
         CascadeType.DETACH : 부모가 영속 해제하면 자식도 같이 영속 해제된다.
         CascadeType.REFRESH : 부모가 재호출(갱신)되면 자식도 같이 재호출(갱신) 한다.
         CascadeType.PERSIST : 부모가 저장 되면 자식도 같이 저장된다.
+
     - fetch : 부모[PK]조회 시, 자식[FK] 관계에서 엔티티 조회여부 선택
         FetchType.EAGER : (기본값) 해당 엔티티 조회시 참조 엔티티 모두 즉시 조회한다.
             - 특징 : 초기 로딩 느리다. 재사용 시 빠르다. *불필요한 정보까지 있을 경우 성능 저하*
         FetchType.LAZY : 해당 엔티티 조회 시 참조 엔티티는 조회하지 않는다. < 참조 엔티티 호출 시 조회 >
             - 특징 : 초기 로딩 빠르다. 재사용성 느리다. *필요한 정보까지만 적절하게 사용* < 지연 로딩 >
+
+   - 단방향 / 양방향 활용
+      - 만약에 1번 카테고리에 등록한다면
+         BoardEntity saveEntity = new BoardEntity();
+
+        // saveEntity.setCategory(1); [x]
+
+         CategoryEntity category = repository.findById(1).get();
+         saveEntity.setCategoryEntity( category ); [o]
+
+        repository.save( saveEntity );
+
+      - 만약에 3번 게시물에 댓글을 등록한다면 ** fk 필드에 fk값인 3 대신에 3을 갖는 fk엔티티 찾아서 대입하자 **
+        ReplyEntity saveEntity = new ReplyEntity();
+            BoardEntity board = repository.findById(3).get();
+        saveEntity.setBoardEntity(board)
+        repository.save(saveEntity)
 */
