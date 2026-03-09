ef943291581ea65b70809d7b25f972b7e856d5fa
dokyung253-lang <dokyung253@gmail.com>
2026-03-09 17:02:14 +0900

    123123

--- src/main/react/src/main.jsx
+++ src/main/react/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+createRoot(document.getElementById('root')).render(<App />)
+
+// [1] index.html(싱글페이지) 에서 root라는 id 갖는 div 요소 호출
+// const root = dovument.querySelector('#root');
+
+// [2] root 마크업에 렌더링(render)
+// createRoot(root).render(<h1>안녕하세요</h1>)
+// vs root.innerHTML = "<h1> 안녕하세요! </h1>" ;
