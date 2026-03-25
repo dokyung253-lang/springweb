c2db3067194f6e6e7b5c79f6be972c8bc750a3d1
dokyung253-lang <dokyung253@gmail.com>
2026-03-25 14:45:04 +0900

    0325 2:44

--- src/main/java/example/day16/FileService.java
+++ src/main/java/example/day16/FileService.java
@@ -3,35 +3,48 @@ package example.day16;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class FileService {
 
     // 업로드 경로 1] 로컬 환경
     private String baseDir = System.getProperty("user.dir"); // 현재 프로젝트명 , C:\Users\임도경\OneDrive\바탕 화면\springweb
-    private String uploadDir = baseDir + "/build/resources/main/static"; // 상세경로 추가
+    private String uploadDir = baseDir + "/build/resources/main/static/upload/"; // 상세경로 추가
     // 업로드 경로 2] 클라우드 환경 * 추후에 *
 
 
     // [1] 업로드
-    public String upload(MultipartFile uploadFile){
+    public String upload(MultipartFile uploadFile) {
 
         // 1)  만약에 파일이 존재하지 않으면
-        if( uploadFile.isEmpty() == true ){ return null; } // 업로드 실패 : 파일 없어서
-        // 2) 업로드할 파일의 경로 잡아줌 * 서버경로 * ,   개발자(src파일) -- 배포/실행--> 서버(build파일) <-- 클라이언트(사용자)
-        String fileName = uploadFile.getOriginalFilename(); // 업로드할 파일명
-        File uploadPath = new File( uploadDir ); // 업로드할 UploadDir+파일명을 file 객체 내 대입
+        if (uploadFile.isEmpty() == true) {return null;} // 업로드 실패 : 파일 없어서
+        // 2) 업로드할 파일의 경로 잡아줌 * 서버의 전체 경로 * ,   개발자(src파일) -- 배포/실행--> 서버(build파일) <-- 클라이언트(사용자)
+        File uploadPath = new File(uploadDir); // 업로드할 UploadDir+파일명을 file 객체 내 대입
         // ** 만약에 해당 경로의 폴더가 존재하지 않으면 폴더 생성
-        if( uploadPath.exists() == false ){// !: 부정문 , : file객체.exist() :경로가 존재하면 false
-            uploadPath.mkdir(); //file객체.mkdir() : 경로/폴더 생성
+        if (uploadPath.exists() == false) {// !: 부정문 , : file객체.exist() :경로가 존재하면 false
+            uploadPath.mkdirs(); //file객체.mkdir() : 경로/폴더 생성
         }
-        // 3) 업로드
-        File uploadRealPath = new File( uploadDir + fileName); // 파일명과 경로 연결해서 최종적인 경로 파일객체 생성
-        try {uploadFile.transferTo( uploadRealPath ); // 업로드파일을 특정한 경로에 전달 * 예외처리 항상 발생 *
-        return fileName;
-        }catch (IOException e){System.out.println(e);}
+        // 3) 업로드, 실제업로드경로 + 파일명
+        // ** 만약에 서로 다른 사람/요청이 동일한 파일명으로 다른 파일 업로드하면 고유식별(무결성) 깨짐
+        // 즉] 파일명은 동일하지만 다른 파일일 수 있다. UUID vs 날짜/시간(밀리초) vs Pk번호 조합
+        String uuid = UUID.randomUUID().toString(); // UUID이란? 중복없는 난수 문자열 생성 함수( 고유성 보장 )
+            // * UUID _ 파일명 : UUID와 파일명 사이에 _언더바 구분하자 (왜? 추후에 분리해야 하므로)
+            // * UUID에는 _언더바가 절대없다. 하지만 파일명에는 _언더바 존재할 수 있다.( 파일명 치환 )
+
+        String fileName = uuid+"_"+uploadFile.getOriginalFilename().replaceAll("_", "-"); // 업로드할 파일명
+
+
+        File uploadRealPath = new File(uploadDir + fileName); // 파일명과 경로 연결해서 최종적인 경로 파일객체 생성
+        try {
+            uploadFile.transferTo(uploadRealPath); // 업로드파일을 특정한 경로에 전달 * 예외처리 항상 발생 *
+            return fileName;
+        } catch (IOException e) {
+            System.out.println(e);
+        }
         return null;
     }
 }
